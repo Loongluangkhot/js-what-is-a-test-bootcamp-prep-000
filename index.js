b58470adf796e9fb5fc58e21a8be1cf6dad3c5cc
Nalongsak Luangkhot <loongluangkhot@gmail.com>
2018-07-13 08:39:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,7 @@
 var name = "Joe"
 var height = 74
 var message = `${name} is ${height} inches tall`
-var message2 = name + " is " + height + " inches tall"
+
 module.exports = {name, height, message}
 
-height = 80
-
 console.log(message)
-console.log(message2)
